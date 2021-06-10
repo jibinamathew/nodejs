daddd0c19eb442f2fb57c4cccad5a31576be7a51
jibinamathew <83585188+jibinamathew@users.noreply.github.com>
2021-06-11 04:39:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 4000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node 12345!\n'
+  const image = <img src=${user.https://static.india.com/wp-content/uploads/2015/11/089-414x246.jpg}/>
   res.end(msg);
 });
 
